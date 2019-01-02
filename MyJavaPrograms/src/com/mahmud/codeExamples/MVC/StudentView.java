006a1b975d4b5371324ba72c7187e9fb1b9cc571
mahmud69 <khalid.mahmud@gmail.com>
2019-01-02 11:11:50 -0500

    MVC Pattern Demo

---
+++ MyJavaPrograms/src/com/mahmud/codeExamples/MVC/StudentView.java
@@ -0,0 +1,9 @@
+package com.mahmud.codeExamples.MVC;
+
+public class StudentView {
+    public void printStudentDetails(String studentName, String id){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("ID: " + id);
+    }
+}
